7c1ae482227ab136933ecca6b7fdd4a7073bff1a
Bilawal Hussain <engrbilawalhussain@gmail.com>
2024-11-23 05:46:39 +0800

    first commit

--- TweetWebApp/TweetWebApp/settings.py
+++ TweetWebApp/TweetWebApp/settings.py
@@ -122,6 +122,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = '/accounts/login'
+
+LOGIN_REDIRECT_URL = '/tweetapp/'
+LOGOUT_REDIRECT_URL = '/tweetapp/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
